3f38eb4cab2a8229ddc28fd095a287138f79409a
Zmej Serow <zmej.serow@gmail.com>
2021-11-13 15:23:30 +0100

    sublist

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,5 +1,17 @@
 (ns sublist)
 
+(defn contained-in [a b]
+  ;; returns true if list a contained in list b
+  (let [len-a (count a), len-b (count b)]
+    (if
+      (< len-b len-a) false
+      (if (= a (take len-a b)) true
+        (contained-in a (rest b))))))
+
 (defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+  (cond
+    (= list1 list2)            :equal
+    (contained-in list2 list1) :superlist
+    (contained-in list1 list2) :sublist
+    :else                      :unequal
+  ))
