3c860ee7529a98f6e3a6b63017c61b0c1c3bf4b4
Zmej Serow <zmej.serow@gmail.com>
2021-11-28 00:09:54 +0100

    reduce vs apply to get exponentiation function

--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -3,7 +3,7 @@
 (defn pow
   "Exponentiation function"
   [x n]
-  (reduce * 1 (repeat n x)))
+  (apply * (repeat n x)))
 
 (defn reducer
   [acc x n]
@@ -19,12 +19,6 @@
              (conj digits (rem num 10))))))
 
 (defn armstrong?
-  "An Armstrong number is a number that is the sum of its own digits each raised to the power of the number of digits.
-   For example:
-   9 is an Armstrong number, because 9 = 9^1 = 9
-   10 is not an Armstrong number, because 10 != 1^2 + 0^2 = 1
-   153 is an Armstrong number, because: 153 = 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
-   154 is not an Armstrong number, because: 154 != 1^3 + 5^3 + 4^3 = 1 + 125 + 64 = 190"
   [num]
   (let [digits       (to-digits num)
         digits-count (count digits)] 
